b5029c9abc37ad5df0bde5b986d6ae898b4f8ec4
David J. Burger <davidburger@gmail.com>
2023-07-17 17:47:43 -0500

    Add american to decimal conversion tests

--- wagering_test.go
+++ wagering_test.go
@@ -5,6 +5,36 @@ import (
 	"testing"
 )
 
-func TestSomething(t *testing.T) {
-	assert.True(t, true, "True is true!")
+func TestConvertAmerican(t *testing.T) {
+	var americanToDecimal = []Odds{
+		{100.0, +9900.0},
+		{4.0, +300.0},
+		{2.5, +150.0},
+		{1.91, -110.0},
+		{1.67, -150.0},
+		{1.33, -300.0},
+		{1.1, -1000.0},
+	}
+	for _, odds := range americanToDecimal {
+		converted := NewOddsFromAmerican(odds.americanOdds)
+		assert.Equal(t, odds.americanOdds, converted.americanOdds, "converting american %v", odds.americanOdds)
+		assert.InDeltaf(t, converted.decimalOdds, odds.decimalOdds, 0.01, "converting american %v", odds.americanOdds)
+	}
+}
+
+func TestConvertDecimal(t *testing.T) {
+	var decimalToAmerican = []Odds{
+		{100.0, +9900.0},
+		{4.0, +300.0},
+		{2.5, +150.0},
+		{1.91, -109.89},
+		{1.67, -149.25},
+		{1.33, -303.03},
+		{1.1, -1000.0},
+	}
+	for _, odds := range decimalToAmerican {
+		converted := NewOddsFromDecimal(odds.decimalOdds)
+		assert.InDeltaf(t, odds.americanOdds, converted.americanOdds, 0.01, "converting decimal %v", odds.decimalOdds)
+		assert.Equal(t, odds.decimalOdds, converted.decimalOdds, "converting decimal %v", odds.decimalOdds)
+	}
 }
