1b5631202643f4f11fef782b289fb32e51930453
David J. Burger <davidburger@gmail.com>
2023-07-17 15:42:15 -0500

    Add Odds related stuff and dummy test
    Add beginning Odds related structs and methods and
    a dummy test.

--- wagering.go
+++ wagering.go
@@ -1,7 +1,35 @@
 package wagering
 
-import "fmt"
+type Odds struct {
+	decimalOdds  float64
+	americanOdds float64
+}
 
-func HelloWorld() {
-	fmt.Println("Hello world")
+func NewOddsFromAmerican(americanOdds float64) Odds {
+	var decimalOdds float64
+	if americanOdds > 0 {
+		decimalOdds = americanOdds/100.0 + 1.0
+	} else {
+		decimalOdds = 1.0 - 100.0/americanOdds
+	}
+	return Odds{decimalOdds: decimalOdds, americanOdds: americanOdds}
+}
+
+func NewOddsFromDecimal(decimalOdds float64) Odds {
+	var americanOdds float64
+	if decimalOdds >= 2.0 {
+		americanOdds = (decimalOdds - 1.0) * 100.0
+	} else {
+		americanOdds = -100.0 / (decimalOdds - 1.0)
+	}
+	return Odds{decimalOdds: decimalOdds, americanOdds: americanOdds}
+}
+
+func average(odds ...Odds) Odds {
+	sum := 0.0
+	for _, odd := range odds {
+		sum += odd.decimalOdds
+	}
+	avg := sum / float64(len(odds))
+	return NewOddsFromDecimal(avg)
 }
